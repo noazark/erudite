e33de395fef217a67bec7853163d4eaa26838a9a
Noah H. Smith <nsmith@rendergardenmedia.com>
2011-09-29 17:56:40 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erudite::Application.config.secret_token = 'fba921fc658a0514b174224b9619e3a7562ac1771d7e695d0d4db7a6f78daaf28e9e5412a0f8bdb0e4adc1178f4f2d38b78c705602855a5c2abe88de17b4d3b6'
