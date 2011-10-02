766afbae1a6e3bab67c5e079f21fd836ebcaacd3
Noah H. Smith <nsmith@rendergardenmedia.com>
2011-10-02 15:36:23 -0500

    Added depth checks back in, but this time it functions differently. Only pass the depth along, cache simply creates an crawl job of itself, therefore, no depth is added.

--- app/workers/cache.rb
+++ app/workers/cache.rb
@@ -3,29 +3,29 @@ require 'open-uri'
 class Cache
   @queue = :cache_queue
 
-  def self.perform(document_uri, force = false)
+  def self.perform(document_uri, depth = 1, force = false)
     start_at = Time.now
+    if depth <= APP_CONFIG[:crawler][:penetration_depth]
+      # Create a document, or find it, based on the passed URI.
+      document = Document.find_or_create_by(uri: document_uri)
 
-    # Create a document, or find it, based on the passed URI.
-    document = Document.find_or_create_by(uri: document_uri)
+      # Check and cancel the crawl if the document has been crawled recently
+      unless force
+        return if document.cached_at && document.cached_at >= Time.now - 6.hours
+      end
 
-    # Check and cancel the crawl if the document has been crawled recently
-    unless force
-      return if document.cached_at && document.cached_at >= Time.now - 6.hours
+      # Grab page contents and remove worthless tags
+      page = Nokogiri::HTML(open(URI.parse(document.uri)))
+      page.xpath("//script").remove
+      page.xpath("//style").remove
+
+      document._cache = page.to_s
+      document.cached_at = Time.now
+
+      document.save!
+
+      Resque.enqueue(Crawl, document.id, depth)
     end
-
-    # Grab page contents and remove worthless tags
-    page = Nokogiri::HTML(open(URI.parse(document.uri)))
-    page.xpath("//script").remove
-    page.xpath("//style").remove
-
-    document._cache = page.to_s
-    document.cached_at = Time.now
-
-    document.save!
-
-    Resque.enqueue(Crawl, document.id)
-
     p "cached: #{document_uri} - #{Time.now - start_at}"
   end
 end
