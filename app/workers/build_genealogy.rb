89c402ae7a6493c3965f983d61dc82d3f86d432d
Noah H. Smith <nsmith@rendergardenmedia.com>
2011-10-03 20:28:36 -0500

    Catching and rescuing document not found errors... should perform some kind of task.

--- app/workers/build_genealogy.rb
+++ app/workers/build_genealogy.rb
@@ -6,8 +6,12 @@ class BuildGenealogy
   def self.perform(document_id, force = false)
     document = Document.find(document_id)
     document.links.each do |link|
-      reference = Document.first(conditions: { uri: link })
-      document.references << reference
+      begin
+        reference = Document.first(conditions: { uri: link })
+        document.references << reference if reference.empty?
+      rescue Mongoid::Errors::DocumentNotFound
+        # rebuild?
+      end
     end
     p "#{document.title}: #{document.references.length} references, #{document.referenced_by.length} referenced_by"
   end
