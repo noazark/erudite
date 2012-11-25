0bcb252bb578cc2f6f1e9eff2900a512bd650e8d
Noah H. Smith <nsmith@rendergardenmedia.com>
2012-11-25 13:54:01 -0600

    stripping down document for rebuild

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,42 +1,9 @@
 class Document
   include Mongoid::Document
-  include Mongoid::Timestamps
-  include Mongoid::Paranoia
 
-  include Tire::Model::Search
-  #include Tire::Model::Callbacks
-
-  field :title, type: String
+  field :_id, type: String, default: ->{ uri }
   field :uri, type: String
-  field :crawl_duration, type: Integer
-  field :links, type: Array
-  field :contents, type: Array
 
-  field:_cache
-
-  field :cached_at, type: DateTime
-  field :crawled_at, type: DateTime
-
-  index_name 'mongo-documents'
-  mapping do
-    indexes :title, :type => 'string', :analyzer => 'snowball', :boost => 100
-    indexes :uri, :type => 'string', :analyzer => 'snowball'
-  end
-
-  def title
-    super || self.uri
-  end
-
-  def self.crawled
-    where :crawled_at.exists => true
-  end
-
-  def to_indexed_json
-    self.to_json
-  end
-
-  def self.paginate(options = {})
-    page(options[:page]).per(options[:per_page])
-  end
+  validates :uri, presence: true
 end
 
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Document do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "overrides the default id" do
+    uri = 'http://example.com'
+    subject = Document.new uri: uri
+    subject.id.should eq uri
+  end
+
+  it "requires a uri" do
+    should validate_presence_of :uri
+  end
+
+  describe "cache" do
+    
+    it "gets the contents at the URI"
+
+  end
+
 end
