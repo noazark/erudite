e55a1abef7b683f5f08e9c3264dffa291826db80
Noah H. Smith <nsmith@rendergardenmedia.com>
2012-11-25 22:23:42 -0600

    completely rebuilt workers, simply deligate to classes and spawn children

--- app/workers/cache.rb
+++ app/workers/cache.rb
@@ -1,38 +1,8 @@
-require 'open-uri'
-
 class Cache
   @queue = :cache_queue
 
-  def self.perform(document_uri, depth = 1, force = false)
-    start_at = Time.now
-    if depth <= APP_CONFIG[:crawler][:penetration_depth]
-      # Create a document, or find it, based on the passed URI.
-      document = Document.find_or_create_by(uri: document_uri)
-
-      # Check and cancel the crawl if the document has been crawled recently
-      if !document.cached_at || document.cached_at <= Time.now - 6.hours
-        # Grab page contents and remove worthless tags
-        begin
-          page = Nokogiri::HTML(open(URI.parse(document.uri)))
-        rescue Net::HTTPServerError, Net::HTTPClientError
-          p "destroy: #{document_uri} - #{Time.now - start_at}"
-          document.destroy
-        rescue
-          p "failed: #{document_uri} - #{Time.now - start_at}"
-          return
-        end
-        page.xpath("//script").remove
-        page.xpath("//style").remove
-
-        document._cache = page.to_s
-        document.cached_at = Time.now
-
-        document.save!
-      end
-
-      Resque.enqueue(Crawl, document.id, depth, force)
-    end
-    p "cached: #{document_uri} - #{Time.now - start_at}"
+  def self.perform(uri, depth = 1)
+    CacheHTTP.perform uri
+    Resque.enqueue(Crawl, uri, depth) unless depth >= 3
   end
 end
-
--- app/workers/crawl.rb
+++ app/workers/crawl.rb
@@ -1,59 +1,11 @@
-require 'open-uri'
-
 class Crawl
   @queue = :crawl_queue
 
-  def self.perform(document_id, depth = 1, force = false)
-    start_at = Time.now
-    if depth <= APP_CONFIG[:crawler][:penetration_depth]
-      # Create a document, or find it, based on the passed URI.
-      document = Document.find(document_id)
+  def self.perform(uri, depth = 1)
+    links = CrawlHTTP.perform uri
 
-      # If the document hasn't been cached recently, cache it.
-      if document.cached_at && document.cached_at <= Time.now - 6.hours
-        Resque.enqueue(Cache, document.uri)
-      else
-        # Grab page contents and remove worthless tags
-        page = Nokogiri::HTML(document._cache)
-
-        # Save to document
-        #
-        # To reduce latency, deffer all possible parsing and interpreting to
-        # separate, threaded, workers. All the content is being saved almost
-        # exactly as it was found on the page, with a few tricks here and there
-        # to break it out into it's pieces
-
-        # Page Title, as found in <head><title>...</title></head>
-        document.title = page.xpath("//title").first.content rescue nil
-
-        # Save all the anchors with href values onto the document
-        document.links = page.xpath("//a").map do |link|
-          link['href'] if link['href']
-        end
-
-        # Save all the contents (words) found on the page onto the document
-        document.contents = page.xpath("//text()").to_s.split
-
-        # Update crawled_at time stamp
-        document.crawled_at = DateTime.now
-        document.crawl_duration = Time.now - start_at
-
-        # Save and throw error, so the job will fail
-        document.save!
-      end
-
-      uri = URI.parse(document.uri)
-
-      document.links.each do |link|
-        begin
-          path = URI.parse(link)
-          path = uri.merge(path) if path.relative?
-          path = URI.escape(path.to_s)
-          Resque.enqueue(Cache, path, depth.next) if depth < APP_CONFIG[:crawler][:penetration_depth]
-        rescue
-        end
-      end
+    links.each do |link|
+      Resque.enqueue(Cache, link, depth.next)
     end
-    p "crawled: #{document.uri} - #{Time.now - start_at}"
   end
 end
